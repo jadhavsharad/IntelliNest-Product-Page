ca9a8814d9d0283a13e8a7b0bedbbe9e7a539dc5
Sharad Jadhav <60263979+jadhavsharad@users.noreply.github.com>
2024-01-17 20:35:17 +0530

    updated

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ], theme: {
+    extend: {
+      fontFamily:{
+        'Inter': ['Inter'],
+        'Allura': ['Allura'],
+        'Foldit': ['Foldit'],
+        'Syne': ['Syne'],
+        'Poppins': ['Poppins'],
+        'Syncopate':['Syncopate']
+        
+      }
+    },
+  },
+  plugins: [],
+}
+
